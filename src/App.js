bc8ddc3b68e6560816b4fc4ca2583ac0d60a7bfb
kyynk <93762645+kyynk@users.noreply.github.com>
2022-11-25 15:40:44 +0800

    lab7

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// import logo from './logo.svg';
+import './App.css';
+import Title from './components/title';
+import MySidebar from './components/sidebar';
+import Main from './components/main';
+
+function App() {
+  return (
+    <div className="App">
+      <Title/>
+      <MySidebar/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
+// <header className="App-header">
+//   <img src={logo} className="App-logo" alt="logo" />
+//   <p>
+//     Edit <code>src/App.js</code> and save to reload.
+//   </p>
+//   <a
+//     className="App-link"
+//     href="https://reactjs.org"
+//     target="_blank"
+//     rel="noopener noreferrer"
+//   >
+//     Learn React
+//   </a>
+// </header>
---
+++ src/components/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Main() {
+    return(
+        <div className="row col-12 d-flex justify-content-center text-balck">
+            <h3>Hi</h3>
+        </div>
+    )
+}
+export default Main;
